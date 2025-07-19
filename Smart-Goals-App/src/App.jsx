c7785dd536e23394326c4765720b4b6e99404a77
cindy kivala <cindykivala@gmail.com>
2025-07-19 12:01:33 +0300

    adding new goals

--- Smart-Goals-App/src/App.jsx
+++ Smart-Goals-App/src/App.jsx
@@ -7,7 +7,10 @@ function App() {
   useEffect(() => {
     fetch('http://localhost:3000/goals')
       .then(response => response.json())
-      .then(data => setGoals(data))
+      .then(data => {
+        console.log('Fetched goals:', data)//confirm goal in console
+        setGoals(data)
+      })
       .catch(error => console.error('Error fetching goals:', error)) // Fetch goals from the JSON server
   }, [])
   
@@ -25,6 +28,42 @@ function App() {
           </li>
         ))}
       </ul>
+
+      <h2>Add Your New Goal!</h2>
+      <form onSubmit={handleAddGoal}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={newGoal.name}
+          onChange={(event) => setNewGoal({...newGoal, name: event.target.value})}
+        />
+           <input
+          type="text"
+          placeholder="Category"
+          value={newGoal.category}
+          onChange={(event) => setNewGoal({...newGoal, category: event.target.value})}
+        />
+           <input
+          type="number"
+          placeholder="Saved Amount"
+          value={newGoal.savedAmount}
+          onChange={(event) => setNewGoal({...newGoal, savedAmount: event.target.value})}
+        />
+           <input
+          type="number"
+          placeholder="Target Amount"
+          value={newGoal.targetAmount}
+          onChange={(event) => setNewGoal({...newGoal, targetAmount: event.target.value})}
+        />
+           <input
+          type="date"
+          value={newGoal.deadline}
+          onChange={(event) => setNewGoal({...newGoal, deadline: event.target.value})}
+        />
+        <button type="submit">
+          Add Your Goal
+        </button>
+      </form>
     </div>
   )
 }
