ed6dd641ae2dad59d5cd17cda4bd4e889ae59fd8
cindy kivala <cindykivala@gmail.com>
2025-07-19 11:37:14 +0300

    App setup

--- Smart-Goals-App/src/App.jsx
+++ Smart-Goals-App/src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [goals, setGoals] = useState([])
 
+  useEffect(() => {
+    fetch('http://localhost:3000/goals')
+      .then(response => response.json())
+      .then(data => setGoals(data))
+      .catch(error => console.error('Error fetching goals:', error)) // Fetch goals from the JSON server
+  }, [])
+  
+  //Display dATA
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <h1>Smart Goals Planner</h1>
+      <ul>
+        {goals.map(goal => (
+          <li key={goal.id}>
+            <h2>{goal.name}</h2>
+            <p>Category: {goal.category}</p>
+            <p>Saved: ${goal.savedAmount} / ${goal.targetAmount} </p>
+            <p>Deadline: {goal.deadline}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
